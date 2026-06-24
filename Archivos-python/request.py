e50fa4eecd970d38493718c34e364139871a7dcb
Pablo Gómez Pérez <pablogomez4@usal.es>
2026-06-24 14:37:55 +0200

    segundo commit

--- Archivos-python/request.py
+++ Archivos-python/request.py
@@ -7,4 +7,8 @@ datos= respuesta.json()
 for post in datos:
     
     if post["userId"] == 1:
-        print(post)
+        print(post["title"])
+        print(post["title"], post["body"])
+        print(f"Título: {post['title']}")
+        print(f"Body: {post['body']}")
+'hpo'
