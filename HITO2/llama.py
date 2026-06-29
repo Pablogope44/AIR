29fd9182f86d380211491775c952a400a697db90
Pablo Gómez Pérez <pablogomez4@usal.es>
2026-06-29 10:57:45 +0200

    Script funciones OLLAMA

--- HITO2/llama.py
+++ HITO2/llama.py
@@ -1,10 +1,26 @@
 import requests
 
 def llamar_llama(prompt):
-    respuesta= requests.post("http://localhost:11434/api/generate", json={ "model":"llama3.2", 
-                                                                          "prompt":prompt,
-                                                                           "stream": False})
-    
+    respuesta = requests.post("http://localhost:11434/api/generate", json={
+        "model":"llama3.2", "prompt": f"Tienes que resumir el siguiente texto:{prompt}","stream":False
+    })
     return respuesta.json()["response"]
 
-print(llamar_llama("dime cual es la capital de francia"))
+
+def clasificar_texto(texto):
+    try:
+        respuesta= requests.post("http://localhost:11434/api/generate",json={
+        "model":"llama3.2", "prompt":f"Clasificame el texto segun tu criterio en positivo negativo o neutro{texto}", "stream":False
+  
+    })
+        return respuesta.json()["response"]
+    except Exception:
+        print("Error al procesar la solicitud")
+
+   
+
+
+if __name__ == "__main__":
+    print(llamar_llama("hola me gustan las patatas, jugar al futbol y videojuegos como el RDR2"))
+    print(clasificar_texto("hola me gustan las patatas, jugar al futbol y videojuegos como el RDR2"))
+    
